70be579e18e6dda10c9bc23a3ef6384d5187c947
dhilipsiva <dhilipsiva@gmail.com>
2015-11-02 17:27:39 +0530

    Commiting Serve script

---
+++ scripts/serve.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# serve.sh
+# Copyright (C) 2015 dhilipsiva <dhilipsiva@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+rvm use 2.1.2
+bundle exec middleman server
